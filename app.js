629559596a9b7d2e454a9f4fcd2cde9fdc7aa1de
MC <mchuttani18@gmail.com>
2018-05-15 15:03:16 -0400

    workshop submission

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// Your code will go here!
+const punchingBag = Vue.component('punching-bag', {
+  data: function () {
+    return {
+      health: 100,
+      ended: false,
+  }},
+  methods: {
+    punch() {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    reset() {
+      this.health = 100;
+      this.ended = false;
+    },
+  },
+  template: `
+    <div class="punching-bag">
+      <div id="bag" v-bind:class="{ burst: ended }"></div>
+
+      <div id="bag-health">
+          <div v-bind:style="{ width: health + '%' }"></div>
+      </div>
+
+      <div id="controls">
+          <button v-on:click="punch" v-show="!ended">Punch</button>
+          <button v-on:click="reset">Reset</button>
+      </div>
+    </div>
+  `,
+});
+const routes = [
+  { path: '/', component: punchingBag, props: { tim: false } },
+  { path: '/tim', component: punchingBag, props: { tim: true } },
+];
+
+const router = new VueRouter({
+  routes, // short for `routes: routes`
+  mode: 'history', // removes the hash from our routes
+});
+new Vue({
+  // This is our root instance.
+  el: '#main-app',
+  router
+});
